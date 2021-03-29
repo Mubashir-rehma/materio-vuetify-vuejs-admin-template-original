5a7add8a121a6e508640eddb187e8a7f9caa0f35
JD Solanki <jdsolanki0001@gmail.com>
2021-03-29 14:35:32 +0530

    feat(dev): Vuetify + Formatters added

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,59 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
+  extends: ['eslint:recommended', 'plugin:vue/recommended', '@vue/airbnb'],
   parserOptions: {
     parser: 'babel-eslint',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+
+    // recommended by Vetur
+    'vue/html-self-closing': 'off',
+
+    // Disable max-len
+    'max-len': 'off',
+
+    // we don't want it
+    semi: ['error', 'never'],
+
+    // add parens ony when required in arrow function
+    'arrow-parens': ['error', 'as-needed'],
+
+    // add new line above comment
+    'lines-around-comment': [
+      'error',
+      {
+        beforeBlockComment: true,
+        beforeLineComment: true,
+        allowBlockStart: true,
+        allowClassStart: true,
+        allowObjectStart: true,
+        allowArrayStart: true,
+      },
+    ],
+
+    // add new line above comment
+    'newline-before-return': 'error',
+
+    // Fix: EsLint no-shadow issue
+    // Reference: https://stackoverflow.com/questions/63961803/eslint-says-all-enums-in-typescript-app-are-already-declared-in-the-upper-scope
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
+
+    // add new line below import
+    'import/newline-after-import': ['error', { count: 1 }],
+
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
   },
-};
+}

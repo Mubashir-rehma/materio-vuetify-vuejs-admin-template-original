42333b3c7afbe75aea97ad6e0dbc46595092643e
Aloksoni <alok@pixinvent.com>
2021-05-24 16:25:56 +0530

    refactor(auth): forgot password  v2 code refactor

--- src/views/pages/authentication/ForgotPassword-v2.vue
+++ src/views/pages/authentication/ForgotPassword-v2.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="auth-wrapper auth-v2">
-    <v-row class="auth-inner">
+    <div class="auth-inner">
       <!-- brand logo -->
-      <a
-        href="/"
+      <router-link
+        to="/"
         class="brand-logo d-flex align-center"
       >
         <div class="logo">
@@ -13,66 +13,67 @@
             src="/logo.svg"
           ></v-img>
         </div>
-        <h2 class="app-title ml-3">Materio</h2>
-      </a>
+        <h2 class="text--primary ml-3">
+          Materio
+        </h2>
+      </router-link>
       <!--/ brand logo -->
 
-      <v-col
-        lg="8"
-        class="d-none d-lg-block position-relative overflow-hidden"
-      >
-        <!-- triangle bg -->
-        <v-img
-          height="362"
-          class="auth-mask-bg"
-          :src="`/images/misc/mask-v2-${$vuetify.theme.dark ? 'dark':'light'}.png`"
-        ></v-img>
-        <!-- tree -->
-        <v-img
-          height="226"
-          width="300"
-          class="auth-tree"
-          src="/images/misc/tree-3.png"
-        ></v-img>
-        <!-- 3d character -->
-        <v-img
-          contain
-          max-width="100%"
-          height="692"
-          class="auth-3d-group"
-          src="/images/3d-characters/illustration-forgot-password.png"
-        ></v-img>
-      </v-col>
-
-      <v-col
-        lg="4"
-        class="d-flex align-center auth-bg"
-      >
+      <v-row class="auth-row ma-0">
         <v-col
-          cols="12"
-          sm="8"
-          md="6"
-          lg="12"
-          class="mx-auto my-auto"
+          lg="8"
+          class="d-none d-lg-block position-relative overflow-hidden"
         >
-          <v-card flat>
-            <v-card-text>
-              <p class="text-2xl font-weight-semibold text--primary mb-0">
-                Forgot Password? 🔒
-              </p>
-              <p class="mt-3 mb-3">
-                Enter your email and we'll send you instructions to reset your password
-              </p>
-            </v-card-text>
+          <!-- triangle bg -->
+          <v-img
+            height="362"
+            class="auth-mask-bg"
+            :src="`/images/misc/mask-v2-${$vuetify.theme.dark ? 'dark':'light'}.png`"
+          ></v-img>
 
-            <!-- login form -->
-            <v-card-text>
-              <v-form class="multi-col-validation">
-                <v-row>
-                  <v-col
-                    cols="12"
-                    class="pb-2"
-                  >
+          <!-- tree -->
+          <v-img
+            height="226"
+            width="300"
+            class="auth-tree"
+            src="/images/misc/tree-3.png"
+          ></v-img>
+
+          <!-- 3d character -->
+          <v-img
+            contain
+            max-width="100%"
+            height="692"
+            class="auth-3d-group"
+            src="/images/3d-characters/illustration-forgot-password.png"
+          ></v-img>
+        </v-col>
+
+        <v-col
+          lg="4"
+          class="d-flex align-center auth-bg"
+        >
+          <v-col
+            cols="12"
+            sm="8"
+            md="6"
+            lg="12"
+            class="mx-auto"
+          >
+            <v-card flat>
+              <v-card-text>
+                <p class="text-2xl font-weight-semibold text--primary mb-2">
+                  Forgot Password? 🔒
+                </p>
+                <p class="mb-2">
+                  Enter your email and we'll send you instructions to reset your password
+                </p>
+              </v-card-text>
+
+              <!-- login form -->
+              <v-card-text>
+                <v-form>
+                  <div class="mb-4">
                     <v-text-field
                       v-model="email"
                       outlined
@@ -80,36 +81,38 @@
                       placeholder="email"
                       hide-details
                     ></v-text-field>
-                  </v-col>
+                  </div>
 
-                  <v-col cols="12">
+                  <div>
                     <v-btn
                       block
                       color="primary"
                     >
                       Send reset link
                     </v-btn>
-                  </v-col>
-                </v-row>
-              </v-form>
-            </v-card-text>
+                  </div>
+                </v-form>
+              </v-card-text>
 
-            <v-card-actions class="d-flex justify-center align-center">
-              <a
-                href="login-v2"
-                class="text-sm"
-              >
-                <v-icon
-                  size="24"
-                  color="primary"
-                >{{ icons.mdiChevronLeft }}</v-icon>
-                <span>Back to login</span>
-              </a>
-            </v-card-actions>
-          </v-card>
+              <v-card-actions class="d-flex justify-center align-center">
+                <router-link
+                  :to="{name:'auth-login-v2'}"
+                  class="d-flex align-center text-sm"
+                >
+                  <v-icon
+                    size="24"
+                    color="primary"
+                  >
+                    {{ icons.mdiChevronLeft }}
+                  </v-icon>
+                  <span>Back to login</span>
+                </router-link>
+              </v-card-actions>
+            </v-card>
+          </v-col>
         </v-col>
-      </v-col>
-    </v-row>
+      </v-row>
+    </div>
   </div>
 </template>
 

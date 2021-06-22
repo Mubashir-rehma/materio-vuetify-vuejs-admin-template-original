2b2420ad5e2650194ba351099b465dae94ff00a9
jdsolanki0001 <jdsolanki0001@gmail.com>
2021-06-22 11:29:29 +0530

    fix(page-error): Icon fix

--- src/views/Error404.vue
+++ src/views/Error404.vue
@@ -12,8 +12,11 @@
     ></v-img>
 
     <div class="page-title text-center">
-      <h2 class="text-2xl font-weight-semibold text--primary">
-        Page Not Found ⚠️
+      <h2 class="text-2xl font-weight-semibold text--primary d-flex align-center justify-center">
+        <span class="mr-2">Page Not Found</span>
+        <v-icon color="warning">
+          {{ icons.mdiAlert }}
+        </v-icon>
       </h2>
       <p class="text-sm">
         we couldn't find the page you are looking for
@@ -36,6 +39,20 @@
     </div>
   </div>
 </template>
+
+<script>
+import { mdiAlert } from '@mdi/js'
+
+export default {
+  setup() {
+    return {
+      icons: {
+        mdiAlert,
+      },
+    }
+  },
+}
+</script>
 
 <style lang="scss">
 @import '@core/preset/preset/misc.scss';

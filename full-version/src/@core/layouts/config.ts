cedbab654b6cd9b26822c101e0fa3f7a439601fe
JD Solanki <jdsolanki0001@gmail.com>
2021-12-30 17:16:02 +0530

    feat(layout): box-sizing added

--- full-version/src/@core/layouts/config.ts
+++ full-version/src/@core/layouts/config.ts
@@ -10,7 +10,7 @@ export const config = ref<Config>({
 
     // logo: () => h('img', { src: 'assets/colored-logo.png' }, null),
     contentWidth: 'boxed',
-    contentLayoutNav: 'horizontal',
+    contentLayoutNav: 'vertical',
     overlayNavFromBreakpoint: breakpointsVuetify.md,
     enableI18n: true,
     isRtl: false,

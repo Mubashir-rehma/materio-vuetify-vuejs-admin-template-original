1487da0f69973926ff443fcfdde70c10cf5c39ca
JD Solanki <jdsolanki0001@gmail.com>
2022-05-12 11:32:53 +0530

    feat(scss): New logical mixin added

--- typescript-version/full-version/src/plugins/vuetify.ts
+++ typescript-version/full-version/src/plugins/vuetify.ts
@@ -1,10 +1,27 @@
 // Styles
 import '@core/vuetify.scss'
 import '@mdi/font/css/materialdesignicons.css'
-
 // Vuetify
+import type { ThemeDefinition } from 'vuetify'
 import { createVuetify } from 'vuetify'
 import { aliases, mdi } from 'vuetify/lib/iconsets/mdi'
+
+
+const myCustomLightTheme: ThemeDefinition = {
+  dark: false,
+  colors: {
+    'background': '#FFFFFF',
+    'surface': '#FFFFFF',
+    'primary': '#000',
+    'primary-darken-1': '#3700B3',
+    'secondary': '#03DAC6',
+    'secondary-darken-1': '#018786',
+    'error': '#B00020',
+    'info': '#2196F3',
+    'success': '#4CAF50',
+    'warning': '#FB8C00',
+  },
+}
 
 export default createVuetify({
   defaults: {
@@ -42,6 +59,12 @@ export default createVuetify({
     aliases,
     sets: {
       mdi,
+    },
+  },
+  theme: {
+    defaultTheme: 'myCustomLightTheme',
+    themes: {
+      myCustomLightTheme,
     },
   },
 })

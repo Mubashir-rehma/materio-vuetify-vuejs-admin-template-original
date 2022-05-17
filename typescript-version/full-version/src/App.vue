df24dd77ee0c7c9fba194cd339b2035892ed20e2
JD Solanki <jdsolanki0001@gmail.com>
2022-05-17 13:32:00 +0530

    fix: RTL enabled via patch due to missing API from vuetify

--- typescript-version/full-version/src/App.vue
+++ typescript-version/full-version/src/App.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
 import { useThemeConfig } from '@core/composable/useThemeConfig'
+import { useLayouts } from '@layouts'
 const { isDark } = useThemeConfig()
+const { isAppRtl } = useLayouts()
 </script>
 
 <template>
-  <v-app :theme="isDark ? 'dark' : 'light'">
+  <!-- TODO: Remove this and use vuetify API for Adding RTL initial value -->
+  <v-app
+    :theme="isDark ? 'dark' : 'light'"
+    :style="{ direction: isAppRtl ? 'rtl' : 'ltr' }"
+  >
     <v-main>
       <router-view />
     </v-main>

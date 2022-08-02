7f5fcbc2a0627bca226c24f8507fc48262805cf8
JD Solanki <jdsolanki0001@gmail.com>
2022-08-02 15:23:36 +0530

    refactor(avatar): size prop usage removed from initials demo

--- typescript-version/full-version/src/views/demos/components/avatar/DemoAvatarInitials.vue
+++ typescript-version/full-version/src/views/demos/components/avatar/DemoAvatarInitials.vue
@@ -3,42 +3,36 @@
     <VAvatar
       color="primary"
       class="v-avatar-light-bg text-primary"
-      size="40"
     >
       <span>PI</span>
     </VAvatar>
     <VAvatar
       color="secondary"
       class="v-avatar-light-bg text-secondary"
-      size="40"
     >
       <span>SE</span>
     </VAvatar>
     <VAvatar
       color="success"
       class="v-avatar-light-bg text-success"
-      size="40"
     >
       <span>SU</span>
     </VAvatar>
     <VAvatar
       color="info"
       class="v-avatar-light-bg text-info"
-      size="40"
     >
       <span>IN</span>
     </VAvatar>
     <VAvatar
       color="warning"
       class="v-avatar-light-bg text-warning"
-      size="40"
     >
       <span>WA</span>
     </VAvatar>
     <VAvatar
       color="error"
       class="v-avatar-light-bg text-error"
-      size="40"
     >
       <span>ER</span>
     </VAvatar>

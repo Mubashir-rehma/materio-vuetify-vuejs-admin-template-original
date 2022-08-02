6198857a4528c09bd1fdca14d4f68dfd448ac29c
JD Solanki <jdsolanki0001@gmail.com>
2022-08-02 12:52:39 +0530

    refactor: UnoCSS config is now have separate file

---
+++ typescript-version/full-version/uno.config.ts
@@ -0,0 +1,55 @@
+import { defineConfig, presetIcons } from 'unocss'
+
+export default defineConfig({
+
+  theme: {
+    fontSize: {
+      'xs': ['0.75rem', '1rem'],
+      'sm': ['0.875rem', '1.25rem'],
+      'base': ['1rem', '1.5rem'],
+      'lg': ['1.125rem', '1.75rem'],
+      'xl': ['1.25rem', '1.75rem'],
+      '2xl': ['1.5rem', '2rem'],
+      '3xl': ['1.875rem', '2.25rem'],
+      '4xl': ['2.25rem', '2.5rem'],
+      '5xl': ['3rem', '1'],
+      '6xl': ['3.75rem', '1'],
+      '7xl': ['4.5rem', '1'],
+      '8xl': ['6rem', '1'],
+      '9xl': ['8rem', '1'],
+    },
+  },
+  rules: [
+    [/^gap-(\d+)$/, ([, d]) => ({ gap: `${Number(d) / 4}rem` })],
+    [/^gap-x-(\d+)$/, ([, d]) => ({ 'column-gap': `${Number(d) / 4}rem` })],
+    [/^gap-y-(\d+)$/, ([, d]) => ({ 'row-gap': `${Number(d) / 4}rem` })],
+    ['truncate', { 'text-overflow': 'ellipsis', 'overflow': 'hidden', 'white-space': 'nowrap' }],
+    [/^text-(\w+)$/, ([, s = 'base'], { theme }) => {
+      const fontSizes = theme.fontSize[s]
+      const themed = Array.isArray(fontSizes) ? fontSizes : [fontSizes]
+
+      if (themed?.[0]) {
+        const [size, height = '1'] = themed
+
+        return {
+          'font-size': size,
+          'line-height': height,
+        }
+      }
+    }],
+  ],
+  presets: [
+    presetIcons({
+      scale: 1.5,
+      extraProperties: {
+        'display': 'inline-block',
+        'vertical-align': 'middle',
+      },
+    }),
+  ],
+  include: [
+    './src/navigation/**',
+    './**/*.vue',
+  ],
+
+})
--- typescript-version/full-version/vite.config.ts
+++ typescript-version/full-version/vite.config.ts
@@ -1,7 +1,6 @@
 import path from 'path'
 import { fileURLToPath } from 'url'
 import VueI18n from '@intlify/vite-plugin-vue-i18n'
-import presetIcons from '@unocss/preset-icons'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import Unocss from 'unocss/vite'
@@ -26,62 +25,7 @@ export default defineConfig({
       autoImport: true,
       styles: 'expose',
     }),
-    Unocss({
-      theme: {
-        fontSize: {
-          'xs': ['0.75rem', '1rem'],
-          'sm': ['0.875rem', '1.25rem'],
-          'base': ['1rem', '1.5rem'],
-          'lg': ['1.125rem', '1.75rem'],
-          'xl': ['1.25rem', '1.75rem'],
-          '2xl': ['1.5rem', '2rem'],
-          '3xl': ['1.875rem', '2.25rem'],
-          '4xl': ['2.25rem', '2.5rem'],
-          '5xl': ['3rem', '1'],
-          '6xl': ['3.75rem', '1'],
-          '7xl': ['4.5rem', '1'],
-          '8xl': ['6rem', '1'],
-          '9xl': ['8rem', '1'],
-        },
-      },
-      rules: [
-        [/^gap-(\d+)$/, ([, d]) => ({ gap: `${Number(d) / 4}rem` })],
-        [/^gap-x-(\d+)$/, ([, d]) => ({ 'column-gap': `${Number(d) / 4}rem` })],
-        [/^gap-y-(\d+)$/, ([, d]) => ({ 'row-gap': `${Number(d) / 4}rem` })],
-        ['truncate', { 'text-overflow': 'ellipsis', 'overflow': 'hidden', 'white-space': 'nowrap' }],
-        [/^text-(\w+)$/, ([, s = 'base'], { theme }) => {
-          const fontSizes = theme.fontSize[s]
-          const themed = Array.isArray(fontSizes) ? fontSizes : [fontSizes]
-
-          if (themed?.[0]) {
-            const [size, height = '1'] = themed
-
-            return {
-              'font-size': size,
-              'line-height': height,
-            }
-          }
-        }],
-      ],
-      presets: [
-        presetIcons({
-          scale: 1.5,
-          extraProperties: {
-            'display': 'inline-block',
-            'vertical-align': 'middle',
-          },
-
-          // collections: {
-          //   mdi: () => import('@iconify-json/mdi/icons.json').then(i => i.default),
-          //   flagpack: () => import('@iconify-json/flagpack/icons.json').then(i => i.default),
-          // },
-        }),
-      ],
-      include: [
-        './src/navigation/**',
-        './**/*.vue',
-      ],
-    }),
+    Unocss(),
     Pages({}),
     Layouts(),
     Components({

298f10c7d4dc95523a9fbf536c180433c0c58bb7
alokVishu <alokpixinvent@gmail.com>
2022-09-13 12:40:26 +0530

    refactor(docs): docs link update in footer

--- typescript-version/full-version/src/layouts/components/Footer.vue
+++ typescript-version/full-version/src/layouts/components/Footer.vue
@@ -29,7 +29,7 @@
         target="noopener noreferrer"
       >More Themes</a>
       <a
-        href="https://demos.themeselection.com/xxx/documentation"
+        href="https://demos.themeselection.com/materio-vuetify-vuejs-admin-template/documentation/"
         target="noopener noreferrer"
       >Documentation</a>
     </span>

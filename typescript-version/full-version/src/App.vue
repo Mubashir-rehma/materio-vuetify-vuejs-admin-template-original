e520fd0a938b7e11da0a74ac638872fc31db3abf
alokVishu <alokpixinvent@gmail.com>
2022-09-13 15:33:56 +0530

    fix(i18n): i18n console issue solved
    close https://github.com/themeselection/master-vue--material/issues/233

--- typescript-version/full-version/src/App.vue
+++ typescript-version/full-version/src/App.vue
@@ -1,20 +1,22 @@
 <script setup lang="ts">
 import BuyNow from '@core/components/BuyNow.vue'
 import { useThemeConfig } from '@core/composable/useThemeConfig'
-const { syncInitialLoaderTheme, syncVuetifyThemeWithTheme: syncConfigThemeWithVuetifyTheme, syncRtlWithLocale } = useThemeConfig()
+import { useLayouts } from '@layouts'
+const { syncInitialLoaderTheme, syncVuetifyThemeWithTheme: syncConfigThemeWithVuetifyTheme } = useThemeConfig()
 
 // ℹ️ Sync current theme with initial loader theme
 syncInitialLoaderTheme()
-
-syncRtlWithLocale()
 syncConfigThemeWithVuetifyTheme()
+const { isAppRtl } = useLayouts()
 </script>
 
 <template>
-  <VApp>
-    <VMain>
-      <RouterView />
-      <BuyNow />
-    </VMain>
-  </VApp>
+  <VLocaleProvider :rtl="isAppRtl">
+    <VApp>
+      <VMain>
+        <RouterView />
+        <BuyNow />
+      </VMain>
+    </VApp>
+  </VLocaleProvider>
 </template>

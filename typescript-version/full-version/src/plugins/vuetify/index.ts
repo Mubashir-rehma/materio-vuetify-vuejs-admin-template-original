ad4a0059038094cd64927b11e3a815fa4281d227
alokVishu <alokpixinvent@gmail.com>
2022-09-14 19:49:29 +0530

    refactor(rtl): unwanted rtl config removed

--- typescript-version/full-version/src/plugins/vuetify/index.ts
+++ typescript-version/full-version/src/plugins/vuetify/index.ts
@@ -1,10 +1,7 @@
-import { useI18n } from 'vue-i18n'
 import { createVuetify } from 'vuetify'
-import { createVueI18nAdapter } from 'vuetify/locale/adapters/vue-i18n'
 import defaults from './defaults'
 import { icons } from './icons'
 import theme from './theme'
-import i18n from '@/plugins/i18n'
 
 // Styles
 import '@core/scss/vuetify/index.scss'
@@ -13,5 +10,4 @@ export default createVuetify({
   defaults,
   icons,
   theme,
-  locale: createVueI18nAdapter({ i18n, useI18n }),
 })

8785b24f73fa99293e9a127e41983281fdb141d6
alokVishu <alokpixinvent@gmail.com>
2022-09-19 18:49:47 +0530

    refactor(route): some route transition removed

--- typescript-version/full-version/src/@core/enums.ts
+++ typescript-version/full-version/src/@core/enums.ts
@@ -4,14 +4,14 @@ export const Skins = {
 } as const
 
 export const RouteTransitions = {
-  'Scroll X': 'scroll-x-transition',
-  'Scroll X Reverse': 'scroll-x-reverse-transition',
-  'Scroll Y': 'scroll-y-transition',
-  'Scroll Y Reverse': 'scroll-y-reverse-transition',
-  'Slide X': 'slide-x-transition',
-  'Slide X Reverse': 'slide-x-reverse-transition',
-  'Slide Y': 'slide-y-transition',
-  'Slide Y Reverse': 'slide-y-reverse-transition',
-  'Fade': 'fade-transition',
-  'None': 'none',
+  // 'Scroll X': 'scroll-x-transition',
+  // 'Scroll X Reverse': 'scroll-x-reverse-transition',
+  // 'Scroll Y': 'scroll-y-transition',
+  // 'Scroll Y Reverse': 'scroll-y-reverse-transition',
+  // 'Slide X': 'slide-x-transition',
+  // 'Slide X Reverse': 'slide-x-reverse-transition',
+  // 'Slide Y': 'slide-y-transition',
+  // 'Slide Y Reverse': 'slide-y-reverse-transition',
+  Fade: 'fade-transition',
+  None: 'none',
 } as const

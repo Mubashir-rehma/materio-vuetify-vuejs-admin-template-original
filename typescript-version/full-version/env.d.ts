49b26ab243e8b1293966ac9d425869e432c60467
JD Solanki <jdsolanki0001@gmail.com>
2022-09-28 16:41:14 +0530

    chore: updated vue shims

--- typescript-version/full-version/env.d.ts
+++ typescript-version/full-version/env.d.ts
@@ -5,13 +5,6 @@
 
 import 'vue-router'
 
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-
-  const component: DefineComponent<{}, {}, any>
-  export default component
-}
-
 declare module 'vue-router' {
   interface RouteMeta {
     action?: string

373aabd64ca2f771b918dfba623f1a7e72175f59
JD Solanki <jdsolanki0001@gmail.com>
2022-11-10 18:17:08 +0530

    feat: vuetify button sizes updated

--- typescript-version/full-version/src/pages/dashboards/analytics.vue
+++ typescript-version/full-version/src/pages/dashboards/analytics.vue
@@ -1,16 +1,98 @@
+<script setup lang="ts">
+const sizes = [
+  'xs-small',
+  'small',
+  'default',
+  'large',
+  'x-large',
+]
+
+const densities = [
+  'default',
+  'comfortable',
+  'compact',
+]
+</script>
+
 <template>
   <div>
-    <p
+    <!--
+      <p
       v-for="i in 1"
       :key="i"
-    >
+      >
       analytics
       {{ String($vuetify.locale.isRtl) }}
-    </p>
+      </p>
+    -->
+    <VCard>
+      <!-- <VDefaultsProvider :defaults="{ VBtn: { color: 'default', variant: 'text' } }"> -->
+      <VCardText>
+        <!-- Size -->
+        <VBtn>
+          Button
+        </VBtn>
+        <VBtn
+          v-for="size in sizes"
+          :key="size"
+          icon="mdi-home"
+          :size="size"
+        />
+
+        <!-- Divider -->
+        <VDivider class="my-4" />
+
+        <!-- Density -->
+        <VBtn>
+          Button
+        </VBtn>
+        <VBtn
+          v-for="density in densities"
+          :key="density"
+          icon="mdi-home"
+          :density="density"
+        />
+
+        <!-- Divider -->
+        <VDivider class="my-4" />
+
+        <!-- Done -->
+        <VBtn
+          icon="mdi-home"
+          density="compact"
+        />
+        <!--
+          <VBtn
+          size="default"
+          icon
+          density="compact"
+          >
+          <VIcon
+          size="22"
+          icon="mdi-home"
+          />
+          </VBtn>
+        -->
+
+        <!-- Done -->
+        <VBtn
+          icon="mdi-home"
+          density="comfortable"
+        />
+
+        <!-- Done -->
+        <VBtn
+          size="large"
+          icon
+          density="compact"
+        >
+          <VIcon
+            size="24"
+            icon="mdi-home"
+          />
+        </VBtn>
+      </VCardText>
+      <!-- </VDefaultsProvider> -->
+    </VCard>
   </div>
 </template>
-
-<route lang="yaml">
-meta:
-  layoutWrapperClasses: layout-content-height-fixed
-</route>

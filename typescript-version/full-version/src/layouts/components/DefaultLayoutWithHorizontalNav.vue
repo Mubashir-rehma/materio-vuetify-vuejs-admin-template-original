eaf0f0fe051a73a038c89034e7eebdcb79a65bf0
alokVishu <alokpixinvent@gmail.com>
2022-11-15 18:08:44 +0530

    chore: rename navbarShortcuts

--- typescript-version/full-version/src/layouts/components/DefaultLayoutWithHorizontalNav.vue
+++ typescript-version/full-version/src/layouts/components/DefaultLayoutWithHorizontalNav.vue
@@ -11,6 +11,7 @@ import { HorizontalNavLayout } from '@layouts'
 import Footer from '@/layouts/components/Footer.vue'
 import NavBarI18n from '@/layouts/components/NavBarI18n.vue'
 import NavBarNotifications from '@/layouts/components/NavBarNotifications.vue'
+import NavbarShortcuts from '@/layouts/components/NavbarShortcuts.vue'
 import NavbarThemeSwitcher from '@/layouts/components/NavbarThemeSwitcher.vue'
 import UserProfile from '@/layouts/components/UserProfile.vue'
 import { VNodeRenderer } from '@layouts/components/VNodeRenderer'
@@ -20,45 +21,6 @@ const { layoutAttrs, injectSkinClasses } = useSkins()
 
 // ℹ️ This will inject classes in body tag for accurate styling
 injectSkinClasses()
-
-const shortcuts = [
-  {
-    icon: 'mdi-calendar',
-    title: 'Calendar',
-    subtitle: 'Appointments',
-    to: { name: 'apps-calendar' },
-  },
-  {
-    icon: 'mdi-file-document-outline',
-    title: 'Invoice App',
-    subtitle: 'Manage Accounts',
-    to: { name: 'apps-invoice-list' },
-  },
-  {
-    icon: 'mdi-account-outline',
-    title: 'Users',
-    subtitle: 'Manage Users',
-    to: { name: 'apps-user-list' },
-  },
-  {
-    icon: 'mdi-view-dashboard-outline',
-    title: 'Dashboard',
-    subtitle: 'Dashboard Analytics',
-    to: { name: 'dashboards-analytics' },
-  },
-  {
-    icon: 'mdi-cog-outline',
-    title: 'Settings',
-    subtitle: 'Account Settings',
-    to: { name: 'pages-account-settings-tab', params: { tab: 'account' } },
-  },
-  {
-    icon: 'mdi-help-circle-outline',
-    title: 'Help Center',
-    subtitle: 'FAQs & Articles',
-    to: { name: 'pages-help-center' },
-  },
-]
 </script>
 
 <template>
@@ -92,7 +54,7 @@ const shortcuts = [
       </VBtn>
       <NavBarI18n />
       <NavbarThemeSwitcher />
-      <AppNavbarShortcuts :shortcuts="shortcuts" />
+      <NavbarShortcuts />
       <NavBarNotifications class="me-2" />
       <UserProfile />
     </template>

8cdb657c9ca8c27c075bd277451ca751f298b9ad
alokVishu <alokpixinvent@gmail.com>
2022-11-17 20:06:47 +0530

    feat: use rgb for `--v-global-theme-primary` color

--- typescript-version/full-version/themeConfig.ts
+++ typescript-version/full-version/themeConfig.ts
@@ -14,8 +14,8 @@ export const { themeConfig, layoutConfig } = defineThemeConfig({
   app: {
     title: 'Materio',
 
-    // ❗ if you have SVG logo and want it to adapt according to theme color, you have to apply color as `color: var(--v-global-theme-primary)`
-    logo: h('div', { innerHTML: logo, style: 'line-height:0; color: var(--v-global-theme-primary)' }),
+    // ❗ if you have SVG logo and want it to adapt according to theme color, you have to apply color as `color: rgb(var(--v-global-theme-primary))`
+    logo: h('div', { innerHTML: logo, style: 'line-height:0; color: rgb(var(--v-global-theme-primary))' }),
     contentWidth: ContentWidth.Boxed,
     contentLayoutNav: AppContentLayoutNav.Vertical,
     overlayNavFromBreakpoint: breakpointsVuetify.md + 16, // 16 for scrollbar. Docs: https://next.vuetifyjs.com/en/features/display-and-platform/

4a95ebef3e629021e88860a7befdbacebce5c16b
alokVishu <alokpixinvent@gmail.com>
2023-01-02 18:36:13 +0530

    refactor(morebtn): minor refactor in more btn

--- typescript-version/full-version/src/@core/components/MoreBtn.vue
+++ typescript-version/full-version/src/@core/components/MoreBtn.vue
@@ -19,7 +19,7 @@ const props = defineProps<Props>()
     >
       <VList
         :items="props.menuList"
-        :item-props="itemProps"
+        :item-props="props.itemProps"
       />
     </VMenu>
   </IconBtn>

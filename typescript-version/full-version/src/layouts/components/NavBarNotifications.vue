76c67e43cc4e773233c1a8d4b69523e5b3ac4241
alokVishu <alokpixinvent@gmail.com>
2023-01-19 16:46:11 +0530

    feat(notification): added some useful feature in notification component

--- typescript-version/full-version/src/layouts/components/NavBarNotifications.vue
+++ typescript-version/full-version/src/layouts/components/NavBarNotifications.vue
@@ -8,41 +8,81 @@ import avatar4 from '@images/avatars/avatar-4.png'
 import avatar5 from '@images/avatars/avatar-5.png'
 import paypal from '@images/svg/paypal.svg'
 
-const notifications: Notification[] = [
+const notifications = ref<Notification[]>([
   {
+    id: 1,
     img: avatar4,
     title: 'Congratulation Flora! 🎉',
     subtitle: 'Won the monthly best seller badge',
     time: 'Today',
+    isRead: true,
   },
   {
+    id: 2,
     text: 'Tom Holland',
     title: 'New user registered.',
     subtitle: '5 hours ago',
     time: 'Yesterday',
+    isRead: true,
   },
   {
+    id: 3,
     img: avatar5,
     title: 'New message received 👋🏻',
     subtitle: 'You have 10 unread messages',
     time: '11 Aug',
+    isRead: true,
   },
   {
+    id: 4,
     img: paypal,
     title: 'Paypal',
     subtitle: 'Received Payment',
     time: '25 May',
+    isRead: true,
     color: 'error',
   },
   {
+    id: 5,
     img: avatar3,
     title: 'Received Order 📦',
     subtitle: 'New order received from john',
     time: '19 Mar',
+    isRead: true,
   },
-]
+])
+
+const removeNotification = (notificationId: number) => {
+  notifications.value.forEach((item, index) => {
+    if (notificationId === item.id)
+      notifications.value.splice(index, 1)
+  })
+}
+
+const markRead = (notificationId: number[]) => {
+  notifications.value.forEach(item => {
+    notificationId.forEach(id => {
+      if (id === item.id)
+        item.isRead = true
+    })
+  })
+}
+
+const markUnRead = (notificationId: number[]) => {
+  notifications.value.forEach(item => {
+    notificationId.forEach(id => {
+      if (id === item.id)
+        item.isRead = false
+    })
+  })
+}
 </script>
 
 <template>
-  <Notifications :notifications="notifications" />
+  <Notifications
+    :notifications="notifications"
+    @remove="removeNotification"
+    @read="markRead"
+    @unread="markUnRead"
+  />
 </template>

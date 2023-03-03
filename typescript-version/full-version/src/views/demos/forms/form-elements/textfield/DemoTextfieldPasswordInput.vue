8fc4ef4b0a5f5ca1982375771ee593d671065b9b
Deep <kumbharedeep85@gmail.com>
2023-03-03 13:35:42 +0530

    refactor(textfield):updated password visibility icon in textfield

--- typescript-version/full-version/src/views/demos/forms/form-elements/textfield/DemoTextfieldPasswordInput.vue
+++ typescript-version/full-version/src/views/demos/forms/form-elements/textfield/DemoTextfieldPasswordInput.vue
@@ -18,7 +18,7 @@ const rules = {
     >
       <VTextField
         v-model="password"
-        :append-inner-icon="show1 ? 'mdi-eye-outline' : 'mdi-eye-off-outline'"
+        :append-inner-icon="show1 ? 'mdi-eye-off-outline' : 'mdi-eye-outline'"
         :rules="[rules.required, rules.min]"
         :type="show1 ? 'text' : 'password'"
         name="input-10-1"

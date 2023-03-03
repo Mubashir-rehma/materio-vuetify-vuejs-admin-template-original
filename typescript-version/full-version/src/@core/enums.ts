db115cac0f8edacc2f2aa216620667b798cca64f
Deep <kumbharedeep85@gmail.com>
2023-03-03 13:27:25 +0530

    refactor(customizer):commented unnecessary route transactions

--- typescript-version/full-version/src/@core/enums.ts
+++ typescript-version/full-version/src/@core/enums.ts
@@ -4,10 +4,11 @@ export const Skins = {
 } as const
 
 export const RouteTransitions = {
-  'Zoom Fade': 'app-transition-zoom-fade',
-  'Fade': 'app-transition-fade',
-  'Fade Bottom': 'app-transition-fade-bottom',
-  'Slide Fade': 'app-transition-slide-fade',
-  'Zoom out': 'app-transition-zoom-out',
-  'None': 'none',
+  // 'Zoom Fade': 'app-transition-zoom-fade',
+  Fade: 'app-transition-fade',
+
+  // 'Fade Bottom': 'app-transition-fade-bottom',
+  // 'Slide Fade': 'app-transition-slide-fade',
+  // 'Zoom out': 'app-transition-zoom-out',
+  None: 'none',
 } as const

2373441c85b74031183cef49162333246d472940
alokVishu <alokpixinvent@gmail.com>
2023-03-07 15:29:26 +0530

    feat(layout): added rgba to hex util function

--- typescript-version/full-version/src/@layouts/utils.ts
+++ typescript-version/full-version/src/@layouts/utils.ts
@@ -88,3 +88,21 @@ export const hexToRgb = (hex: string) => {
 
   return result ? `${parseInt(result[1], 16)},${parseInt(result[2], 16)},${parseInt(result[3], 16)}` : null
 }
+
+/**
+ ** RGBA color to Hex color with / without opacity
+ */
+export const rgbaToHex = (rgba: string, forceRemoveAlpha = false) => {
+  return (
+    `#${
+      rgba
+        .replace(/^rgba?\(|\s+|\)$/g, '') // Get's rgba / rgb string values
+        .split(',') // splits them at ","
+        .filter((string, index) => !forceRemoveAlpha || index !== 3)
+        .map(string => parseFloat(string)) // Converts them to numbers
+        .map((number, index) => (index === 3 ? Math.round(number * 255) : number)) // Converts alpha to 255 number
+        .map(number => number.toString(16)) // Converts numbers to hex
+        .map(string => (string.length === 1 ? `0${string}` : string)) // Adds 0 when length of one number is 1
+        .join('')}`
+  )
+}

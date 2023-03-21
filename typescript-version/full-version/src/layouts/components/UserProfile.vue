ce43db7489f2c8d4c3eeec3393ab03b24237e6c5
Deep Kumbhare <kumbharedeep85@gmail.com>
2023-03-21 11:56:29 +0530

    refactor(userProfile):updated userProfile menu. ## added new Options and also removed hard coded method

--- typescript-version/full-version/src/layouts/components/UserProfile.vue
+++ typescript-version/full-version/src/layouts/components/UserProfile.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { PerfectScrollbar } from 'vue3-perfect-scrollbar'
 import { initialAbility } from '@/plugins/casl/ability'
 import { useAppAbility } from '@/plugins/casl/useAppAbility'
 
@@ -24,6 +25,19 @@ const logout = () => {
       ability.update(initialAbility)
     })
 }
+
+const userProfileList = [
+  { type: 'divider' },
+  { type: 'navItem', icon: 'mdi-account-outline', title: 'Profile', to: { name: 'apps-user-view-id', params: { id: 21 } } },
+  { type: 'navItem', icon: 'mdi-cog-outline', title: 'settings', to: { name: 'pages-account-settings-tab', params: { tab: 'account' } } },
+  { type: 'navItem', icon: 'mdi-credit-card-outline', title: 'Billing', to: { name: 'pages-account-settings-tab', params: { tab: 'billing-plans' } }, badgeProps: { color: 'light-error', content: '3', textColor: 'error' } },
+  { type: 'divider' },
+  { type: 'navItem', icon: 'mdi-lifebuoy', title: 'Help', to: { name: 'pages-help-center' } },
+  { type: 'navItem', icon: 'mdi-currency-usd', title: 'Pricing', to: { name: 'pages-pricing' } },
+  { type: 'navItem', icon: 'mdi-help-circle-outline', title: 'FAQ', to: { name: 'pages-faq' } },
+  { type: 'divider' },
+  { type: 'navItem', icon: 'mdi-logout', title: 'Logout', onClick: logout },
+]
 </script>
 
 <template>
@@ -56,114 +70,76 @@ const logout = () => {
         location="bottom end"
         offset="14px"
       >
-        <VList>
-          <!-- 👉 User Avatar & Name -->
-          <VListItem>
-            <template #prepend>
-              <VListItemAction start>
-                <VBadge
-                  dot
-                  location="bottom right"
-                  offset-x="3"
-                  offset-y="3"
-                  color="success"
-                >
-                  <VAvatar
-                    color="primary"
-                    variant="tonal"
+        <PerfectScrollbar :options="{ wheelPropagation: false }">
+          <VList>
+            <!-- 👉 User Avatar & Name -->
+            <VListItem>
+              <template #prepend>
+                <VListItemAction start>
+                  <VBadge
+                    dot
+                    location="bottom right"
+                    offset-x="3"
+                    offset-y="3"
+                    color="success"
                   >
-                    <VImg
-                      v-if="userData && userData.avatar"
-                      :src="userData.avatar"
-                    />
-                    <VIcon
-                      v-else
-                      icon="mdi-account-outline"
-                    />
-                  </VAvatar>
-                </VBadge>
-              </VListItemAction>
-            </template>
+                    <VAvatar
+                      color="primary"
+                      variant="tonal"
+                    >
+                      <VImg
+                        v-if="userData && userData.avatar"
+                        :src="userData.avatar"
+                      />
+                      <VIcon
+                        v-else
+                        icon="mdi-account-outline"
+                      />
+                    </VAvatar>
+                  </VBadge>
+                </VListItemAction>
+              </template>
 
-            <VListItemTitle class="font-weight-medium">
-              {{ userData.fullName || userData.username }}
-            </VListItemTitle>
-            <VListItemSubtitle>{{ userData.role }}</VListItemSubtitle>
-          </VListItem>
+              <VListItemTitle class="font-weight-medium">
+                {{ userData.fullName || userData.username }}
+              </VListItemTitle>
+              <VListItemSubtitle>{{ userData.role }}</VListItemSubtitle>
+            </VListItem>
 
-          <VDivider class="my-2" />
+            <template
+              v-for="item in userProfileList"
+              :key="item.title"
+            >
+              <VListItem
+                v-if="item.type === 'navItem'"
+                :to="item.to"
+                @click="item.onClick && item.onClick()"
+              >
+                <template #prepend>
+                  <VIcon
+                    class="me-2"
+                    :icon="item.icon"
+                    size="22"
+                  />
+                </template>
 
-          <!-- 👉 Profile -->
-          <VListItem :to="{ name: 'apps-user-view-id', params: { id: 21 } }">
-            <template #prepend>
-              <VIcon
-                class="me-2"
-                icon="mdi-account-outline"
-                size="22"
+                <VListItemTitle>{{ item.title }}</VListItemTitle>
+
+                <template
+                  v-if="item.badgeProps"
+                  #append
+                >
+                  <VBadge v-bind="item.badgeProps" />
+                </template>
+              </VListItem>
+
+              <VDivider
+                v-else
+                class="my-2"
               />
             </template>
-
-            <VListItemTitle>Profile</VListItemTitle>
-          </VListItem>
-
-          <!-- 👉 Settings -->
-          <VListItem :to="{ name: 'pages-account-settings-tab', params: { tab: 'account' } }">
-            <template #prepend>
-              <VIcon
-                class="me-2"
-                icon="mdi-cog-outline"
-                size="22"
-              />
-            </template>
-
-            <VListItemTitle>Settings</VListItemTitle>
-          </VListItem>
-
-          <!-- 👉 Pricing -->
-          <VListItem :to="{ name: 'pages-pricing' }">
-            <template #prepend>
-              <VIcon
-                class="me-2"
-                icon="mdi-currency-usd"
-                size="22"
-              />
-            </template>
-
-            <VListItemTitle>Pricing</VListItemTitle>
-          </VListItem>
-
-          <!-- 👉 FAQ -->
-          <VListItem :to="{ name: 'pages-faq' }">
-            <template #prepend>
-              <VIcon
-                class="me-2"
-                icon="mdi-help-circle-outline"
-                size="22"
-              />
-            </template>
-
-            <VListItemTitle>FAQ</VListItemTitle>
-          </VListItem>
-
-          <!-- Divider -->
-          <VDivider class="my-2" />
-
-          <!-- 👉 Logout -->
-          <VListItem
-            link
-            @click="logout"
-          >
-            <template #prepend>
-              <VIcon
-                class="me-2"
-                icon="mdi-logout"
-                size="22"
-              />
-            </template>
-
-            <VListItemTitle>Logout</VListItemTitle>
-          </VListItem>
-        </VList>
+          </VList>
+        </PerfectScrollbar>
       </VMenu>
       <!-- !SECTION -->
     </VAvatar>

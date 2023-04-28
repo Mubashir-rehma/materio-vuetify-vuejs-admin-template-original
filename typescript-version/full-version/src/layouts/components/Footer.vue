126f68a3d750f461a518604f24212d2bf3e39f9b
Deep Kumbhare <kumbharedeep85@gmail.com>
2023-04-28 18:19:04 +0530

    refactor(tour):refactored tour steps.

--- typescript-version/full-version/src/layouts/components/Footer.vue
+++ typescript-version/full-version/src/layouts/components/Footer.vue
@@ -21,6 +21,7 @@
     <!-- 👉 Footer: right content -->
     <span class="d-md-flex gap-x-4 text-primary d-none">
       <a
+        id="license-btn"
         href="https://themeselection.com/license/"
         target="noopener noreferrer"
       >License</a>
--- typescript-version/full-version/src/pages/extensions/tour.vue
+++ typescript-version/full-version/src/pages/extensions/tour.vue
@@ -58,49 +58,6 @@ onMounted(() => {
       ],
     },
     {
-      id: display.mdAndDown.value ? 'navigationMobile' : 'navigation',
-      title: 'Navigation Menu',
-      arrow: true,
-      attachTo: { element: display.mdAndDown.value ? verticalNavToggleBtn : navMenu, on: display.mdAndDown.value ? 'bottom' : appContentLayoutNav.value === 'horizontal' ? 'bottom' : 'right' },
-      text: display.mdAndDown.value ? 'open the navigation menu by clicking on this icon' : 'You can navigate in admin template via navigation menu',
-      buttons: [
-        {
-          label: 'Back',
-          text: 'Back',
-          action: tour.back,
-          classes: 'backBtnClass',
-        },
-        {
-          label: 'Next',
-          text: 'Next',
-          action: tour.next,
-          classes: 'nextBtnClass',
-        },
-      ],
-    },
-    {
-      id: 'dashboard',
-      title: 'Dashboard',
-      arrow: true,
-      attachTo: { element: document.querySelector('.nav-group'), on: appContentLayoutNav.value === 'horizontal' ? 'bottom' : 'right' },
-      text: 'See your product in action with a live dashboard featuring important metrics.',
-      buttons: [
-        {
-          label: 'Back',
-          text: 'Back',
-          action: tour.back,
-          classes: 'backBtnClass',
-        },
-        {
-          label: 'Next',
-          text: 'Next',
-          action: tour.next,
-          classes: 'nextBtnClass',
-        },
-      ],
-      showOn: () => display.lgAndUp.value,
-    },
-    {
       id: 'notification',
       title: 'Notifications',
       arrow: true,
@@ -122,11 +79,11 @@ onMounted(() => {
       ],
     },
     {
-      id: 'buyNow',
-      attachTo: { element: document.querySelector('#buy-now-btn'), on: 'top' },
+      id: 'upgrade',
+      title: 'Upgrade',
       arrow: true,
-      title: 'Buy Now',
-      text: 'Ready to take your business to the next level? Buy now and start your journey with us!',
+      attachTo: { element: document.querySelector('#license-btn'), on: 'bottom' },
+      text: 'Click here to upgrade plan',
       buttons: [
         {
           label: 'Back',
@@ -135,22 +92,21 @@ onMounted(() => {
           classes: 'backBtnClass',
         },
         {
-          label: 'Next',
-          text: 'Next',
-          action: tour.next,
+          label: 'Finish',
+          text: 'Finish',
+          action: tour.complete,
           classes: 'nextBtnClass',
         },
       ],
     },
+
   ])
 })
 </script>
 
 <template>
   <div>
-    <VCard
-      title="Tour"
-    >
+    <VCard title="Tour">
       <VCardText>
         <VBtn
           variant="outlined"

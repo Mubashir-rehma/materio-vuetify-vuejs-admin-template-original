b11c8adab8f51ead1fb26fbbb697d350187eb88c
jd-solanki <jdsolanki0001@gmail.com>
2023-05-02 13:41:33 +0530

    chore: vertical nav links updated

--- typescript-version/full-version/src/navigation/vertical/app-and-pages.ts
+++ typescript-version/full-version/src/navigation/vertical/app-and-pages.ts
@@ -42,64 +42,16 @@ export default [
       { title: 'Permissions', to: 'apps-permissions' },
     ],
   },
+
   {
     title: 'Pages',
     icon: { icon: 'mdi-file-outline' },
     children: [
-      {
-        title: 'Authentication',
-        children: [
-          {
-            title: 'Login',
-            children: [
-              { title: 'Login v1', to: 'pages-authentication-login-v1', target: '_blank' },
-              { title: 'Login v2', to: 'pages-authentication-login-v2', target: '_blank' },
-            ],
-          },
-          {
-            title: 'Register',
-            children: [
-              { title: 'Register v1', to: 'pages-authentication-register-v1', target: '_blank' },
-              { title: 'Register v2', to: 'pages-authentication-register-v2', target: '_blank' },
-              { title: 'Register Multi-Steps', to: 'pages-authentication-register-multi-steps', target: '_blank' },
-            ],
-          },
-          {
-            title: 'Verify Email',
-            children: [
-              { title: 'Verify Email v1', to: 'pages-authentication-verify-email-v1', target: '_blank' },
-              { title: 'Verify Email v2', to: 'pages-authentication-verify-email-v2', target: '_blank' },
-            ],
-          },
-          {
-            title: 'Forgot Password',
-            children: [
-              { title: 'Forgot Password v1', to: 'pages-authentication-forgot-password-v1', target: '_blank' },
-              { title: 'Forgot Password v2', to: 'pages-authentication-forgot-password-v2', target: '_blank' },
-            ],
-          },
-          {
-            title: 'Reset Password',
-            children: [
-              { title: 'Reset Password v1', to: 'pages-authentication-reset-password-v1', target: '_blank' },
-              { title: 'Reset Password v2', to: 'pages-authentication-reset-password-v2', target: '_blank' },
-            ],
-          },
-          {
-            title: 'Two Steps',
-            children: [
-              { title: 'Two Steps v1', to: 'pages-authentication-two-steps-v1', target: '_blank' },
-              { title: 'Two Steps v2', to: 'pages-authentication-two-steps-v2', target: '_blank' },
-            ],
-          },
-        ],
-      },
       { title: 'Help Center', to: 'pages-help-center' },
       { title: 'User Profile', to: { name: 'pages-user-profile-tab', params: { tab: 'profile' } } },
       { title: 'Account Settings', to: { name: 'pages-account-settings-tab', params: { tab: 'account' } } },
       { title: 'Pricing', to: 'pages-pricing' },
       { title: 'FAQ', to: 'pages-faq' },
-      { title: 'Dialog Examples', to: 'pages-dialog-examples' },
       {
         title: 'Miscellaneous',
         children: [
@@ -113,6 +65,55 @@ export default [
     ],
   },
   {
+    title: 'Authentication',
+    icon: { icon: 'mdi-lock-outline' },
+    children: [
+      {
+        title: 'Login',
+        children: [
+          { title: 'Login v1', to: 'pages-authentication-login-v1', target: '_blank' },
+          { title: 'Login v2', to: 'pages-authentication-login-v2', target: '_blank' },
+        ],
+      },
+      {
+        title: 'Register',
+        children: [
+          { title: 'Register v1', to: 'pages-authentication-register-v1', target: '_blank' },
+          { title: 'Register v2', to: 'pages-authentication-register-v2', target: '_blank' },
+          { title: 'Register Multi-Steps', to: 'pages-authentication-register-multi-steps', target: '_blank' },
+        ],
+      },
+      {
+        title: 'Verify Email',
+        children: [
+          { title: 'Verify Email v1', to: 'pages-authentication-verify-email-v1', target: '_blank' },
+          { title: 'Verify Email v2', to: 'pages-authentication-verify-email-v2', target: '_blank' },
+        ],
+      },
+      {
+        title: 'Forgot Password',
+        children: [
+          { title: 'Forgot Password v1', to: 'pages-authentication-forgot-password-v1', target: '_blank' },
+          { title: 'Forgot Password v2', to: 'pages-authentication-forgot-password-v2', target: '_blank' },
+        ],
+      },
+      {
+        title: 'Reset Password',
+        children: [
+          { title: 'Reset Password v1', to: 'pages-authentication-reset-password-v1', target: '_blank' },
+          { title: 'Reset Password v2', to: 'pages-authentication-reset-password-v2', target: '_blank' },
+        ],
+      },
+      {
+        title: 'Two Steps',
+        children: [
+          { title: 'Two Steps v1', to: 'pages-authentication-two-steps-v1', target: '_blank' },
+          { title: 'Two Steps v2', to: 'pages-authentication-two-steps-v2', target: '_blank' },
+        ],
+      },
+    ],
+  },
+  {
     title: 'Wizard Examples',
     icon: { icon: 'mdi-ray-start-vertex-end' },
     children: [
@@ -120,5 +121,10 @@ export default [
       { title: 'Property Listing', to: { name: 'wizard-examples-property-listing' } },
       { title: 'Create Deal', to: { name: 'wizard-examples-create-deal' } },
     ],
+  },
+  {
+    title: 'Dialog Examples',
+    icon: { icon: 'mdi-square-outline' },
+    to: 'pages-dialog-examples',
   },
 ]

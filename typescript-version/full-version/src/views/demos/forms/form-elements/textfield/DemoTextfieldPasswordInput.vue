6c77564cc9eb70cfb561762c07eb354b05342e55
Deep Kumbhare <kumbharedeep85@gmail.com>
2023-05-19 18:34:45 +0530

    refactor(textfield): refactor text field icons.

--- typescript-version/full-version/src/views/demos/forms/form-elements/textfield/DemoTextfieldPasswordInput.vue
+++ typescript-version/full-version/src/views/demos/forms/form-elements/textfield/DemoTextfieldPasswordInput.vue
@@ -18,7 +18,7 @@ const rules = {
     >
       <VTextField
         v-model="password"
-        :append-inner-icon="show1 ? 'mdi-eye-off-outline' : 'mdi-eye-outline'"
+        :append-inner-icon="show1 ? 'mdi-eye-outline' : 'mdi-eye-off-outline'"
         :rules="[rules.required, rules.min]"
         :type="show1 ? 'text' : 'password'"
         name="input-10-1"
@@ -36,7 +36,7 @@ const rules = {
     >
       <VTextField
         v-model="confirmPassword"
-        :append-inner-icon="show2 ? 'mdi-eye-off-outline' : 'mdi-eye-outline'"
+        :append-inner-icon="show2 ? 'mdi-eye-outline' : 'mdi-eye-off-outline'"
         :rules="[rules.required, rules.min]"
         :type="show2 ? 'text' : 'password'"
         name="input-10-2"

eb85ab84f18bb3fe6ada05aaefdd8292b473a5c1
alokVishu <alokpixinvent@gmail.com>
2023-05-29 12:58:41 +0530

    fix(verticalNav): header translation issue fixed

--- typescript-version/full-version/src/@layouts/components/VerticalNavSectionTitle.vue
+++ typescript-version/full-version/src/@layouts/components/VerticalNavSectionTitle.vue
@@ -23,15 +23,14 @@ const shallRenderIcon = isVerticalNavMini(windowWidth)
         name="vertical-nav-section-title"
         mode="out-in"
       >
-        <!-- eslint-disable vue/no-v-text-v-html-on-component -->
         <Component
           :is="shallRenderIcon ? config.app.iconRenderer : config.app.enableI18n ? 'i18n-t' : 'span'"
           :key="shallRenderIcon"
           :class="shallRenderIcon ? 'placeholder-icon' : 'title-text'"
           v-bind="{ ...config.icons.sectionTitlePlaceholder, ...dynamicI18nProps(item.heading, 'span') }"
-          v-text="!shallRenderIcon ? item.heading : null"
-        />
-        <!-- eslint-enable vue/no-v-text-v-html-on-component -->
+        >
+          {{ !shallRenderIcon ? item.heading : null }}
+        </Component>
       </Transition>
     </div>
   </li>

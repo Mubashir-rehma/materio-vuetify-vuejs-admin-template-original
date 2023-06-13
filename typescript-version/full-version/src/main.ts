cded161536a849a51f8a968b4f3b42f30e5bb702
Deep Kumbhare <kumbharedeep85@gmail.com>
2023-06-13 18:47:35 +0530

    refactor(multi): updated iconify as per nuxt structure.

--- typescript-version/full-version/src/main.ts
+++ typescript-version/full-version/src/main.ts
@@ -1,7 +1,7 @@
 /* eslint-disable import/order */
 import '@/@fake-db/db'
-import '@/@iconify/icons-bundle'
 import App from '@/App.vue'
+import '@/assets/icons/index'
 import ability from '@/plugins/casl/ability'
 import i18n from '@/plugins/i18n'
 import layoutsPlugin from '@/plugins/layouts'

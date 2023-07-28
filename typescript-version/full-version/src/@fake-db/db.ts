cf755234ff37be102c6935216c81fe15bfb62be7
Deep Kumbhare <kumbharedeep85@gmail.com>
2023-07-28 16:36:34 +0530

    chore: removed logistic store and fake db api

--- typescript-version/full-version/src/@fake-db/db.ts
+++ typescript-version/full-version/src/@fake-db/db.ts
@@ -14,7 +14,6 @@ import './apps/chat'
 import './apps/ecommerce'
 import './apps/email'
 import './apps/invoice'
-import './apps/logistics'
 import './apps/permissions'
 
 // forwards the matched request over network

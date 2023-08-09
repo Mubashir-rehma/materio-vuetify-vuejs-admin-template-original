218cf671a6b39ddb9e1c3a4b65c451a37c53c330
Deep Kumbhare <kumbharedeep85@gmail.com>
2023-08-09 12:15:19 +0530

    chore: refactored app.vue in rtl

--- typescript-version/full-version/src/App.vue
+++ typescript-version/full-version/src/App.vue
@@ -16,7 +16,7 @@ handleSkinChanges()
 </script>
 
 <template>
-  <VLocaleProvider rtl>
+  <VLocaleProvider :rtl="isAppRtl">
     <!-- ℹ️ This is required to set the background color of active nav link based on currently active global theme's primary -->
     <VApp :style="`--v-global-theme-primary: ${hexToRgb(global.current.value.colors.primary)}`">
       <RouterView />

a675b84544e5fb70f35a62d23055fe841729fff1
alokVishu <alokpixinvent@gmail.com>
2023-08-15 12:21:33 +0530

    fix(i18n): initialize i18n with vuetify

--- typescript-version/full-version/src/plugins/vuetify/index.ts
+++ typescript-version/full-version/src/plugins/vuetify/index.ts
@@ -1,10 +1,12 @@
 import type { App } from 'vue'
-
+import { useI18n } from 'vue-i18n'
 import { createVuetify } from 'vuetify'
 import { VBtn } from 'vuetify/components/VBtn'
+import { createVueI18nAdapter } from 'vuetify/locale/adapters/vue-i18n'
 import defaults from './defaults'
 import { icons } from './icons'
 import theme from './theme'
+import { i18n } from '@/plugins/i18n/index'
 
 // Styles
 import '@core/scss/template/libs/vuetify/index.scss'
@@ -17,6 +19,9 @@ const vuetify = createVuetify({
   defaults,
   icons,
   theme,
+  locale: {
+    adapter: createVueI18nAdapter({ i18n, useI18n }),
+  },
 })
 
 export default function (app: App) {

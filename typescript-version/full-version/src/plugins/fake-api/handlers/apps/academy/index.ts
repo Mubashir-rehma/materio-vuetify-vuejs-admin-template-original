bc6bf73e5c098e2776dc86846475286c542a5201
Deep Kumbhare <kumbharedeep85@gmail.com>
2023-08-24 19:18:11 +0530

    fix(course-table): fixed course courses table.

--- typescript-version/full-version/src/plugins/fake-api/handlers/apps/academy/index.ts
+++ typescript-version/full-version/src/plugins/fake-api/handlers/apps/academy/index.ts
@@ -10,7 +10,7 @@ export const handlerAppsAcademy = [
   // 👉 Course
   rest.get(buildURL('apps/academy/courses'), (req, res, ctx) => {
     const q = req.url.searchParams.get('q')
-    const label = req.url.searchParams.get('label')
+    const label = req.url.searchParams.get('label') || 'All Courses'
     const hideCompleted = req.url.searchParams.get('hideCompleted')
     const page = req.url.searchParams.get('page')
     const itemsPerPage = req.url.searchParams.get('itemsPerPage')
@@ -34,8 +34,6 @@ export const handlerAppsAcademy = [
 
     const itemsPerPageLocal = is.number(parsedItemsPerPage) ? parsedItemsPerPage : 10
     const pageLocal = is.number(parsedPage) ? parsedPage : 1
-
-    console.log(label)
 
     const filteredCourses = db.courses.filter(course => {
       return (

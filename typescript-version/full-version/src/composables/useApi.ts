4063a8c48008aea7bd54a74a3342cdb4076478b1
Deep Kumbhare <kumbharedeep85@gmail.com>
2023-10-11 11:10:48 +0530

    chore: added authorization header in useApi & $api

--- typescript-version/full-version/src/composables/useApi.ts
+++ typescript-version/full-version/src/composables/useApi.ts
@@ -10,7 +10,19 @@ export const useApi = createFetch({
   },
   options: {
     refetch: true,
+    async beforeFetch({ options }) {
+      const accessToken = useCookie('accessToken').value
 
+      if (accessToken) {
+        options.headers = {
+          ...options.headers,
+          Authorization: `Bearer ${accessToken}`,
+
+        }
+      }
+
+      return { options }
+    },
     afterFetch(ctx) {
       const { data, response } = ctx
 

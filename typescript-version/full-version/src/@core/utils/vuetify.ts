4c1115bb283776a0b9dd6d43640cc2da631eeed2
jd-solanki <jdsolanki0001@gmail.com>
2023-10-13 19:03:47 +0530

    fix: detect system correctly

--- typescript-version/full-version/src/@core/utils/vuetify.ts
+++ typescript-version/full-version/src/@core/utils/vuetify.ts
@@ -1,12 +1,12 @@
-import { namespaceConfig } from '@layouts/stores/config'
+import { cookieRef, namespaceConfig } from '@layouts/stores/config'
 import { themeConfig } from '@themeConfig'
 
 export const resolveVuetifyTheme = (): 'light' | 'dark' => {
-  const isDarkPreferred = usePreferredDark()
+  const cookieColorScheme = cookieRef<'light' | 'dark'>('color-scheme', usePreferredDark().value ? 'dark' : 'light')
   const storedTheme = useCookie(namespaceConfig('theme'), { default: () => themeConfig.app.theme }).value
 
   return storedTheme === 'system'
-    ? isDarkPreferred.value
+    ? cookieColorScheme.value
       ? 'dark'
       : 'light'
     : storedTheme as 'light' | 'dark'

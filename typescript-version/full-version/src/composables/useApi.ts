dc7235550539f811842543f8b89d4b42351a7de7
jd-solanki <jdsolanki0001@gmail.com>
2023-10-17 16:49:10 +0530

    fix: handle empty string in API base URL

--- typescript-version/full-version/src/composables/useApi.ts
+++ typescript-version/full-version/src/composables/useApi.ts
@@ -2,7 +2,7 @@ import { createFetch } from '@vueuse/core'
 import { destr } from 'destr'
 
 export const useApi = createFetch({
-  baseUrl: import.meta.env.VITE_API_BASE_URL ?? '/api',
+  baseUrl: import.meta.env.VITE_API_BASE_URL || '/api',
   fetchOptions: {
     headers: {
       Accept: 'application/json',

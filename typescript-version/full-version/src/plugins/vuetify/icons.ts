2363808060caf2da06cd06621112246bb322a59f
Deep Kumbhare <kumbharedeep85@gmail.com>
2023-10-27 12:07:29 +0530

    refactor(icons): updated vuetify icons.

--- typescript-version/full-version/src/plugins/vuetify/icons.ts
+++ typescript-version/full-version/src/plugins/vuetify/icons.ts
@@ -1,13 +1,44 @@
-import { aliases } from 'vuetify/iconsets/mdi'
+import type { IconAliases } from 'vuetify'
 
-const alertTypeIcon = {
-  success: 'mdi-check-circle-outline',
-  info: 'mdi-information-outline',
-  warning: 'mdi-alert-outline',
-  error: 'mdi-alert-circle-outline',
+const aliases: IconAliases = {
+  info: 'ri-error-warning-line',
+  success: 'ri-checkbox-circle-line',
+  warning: 'ri-alert-line',
+  error: 'ri-error-warning-line',
+  calendar: 'ri-calendar-2-line',
+  collapse: 'ri-arrow-up-s-line',
+  complete: 'ri-check-line',
+  cancel: 'ri-close-line',
+  close: 'ri-close-line',
+  delete: 'ri-close-line',
+  clear: 'ri-close-line',
+  prev: 'ri-arrow-left-s-line',
+  next: 'ri-arrow-right-s-line',
+  checkboxOn: 'custom-checkbox-checked',
+  checkboxOff: 'custom-checkbox-unchecked',
+  checkboxIndeterminate: 'custom-checkbox-indeterminate',
+  delimiter: 'ri-circle-line',
+  sort: 'ri-arrow-up-line',
+  expand: 'ri-arrow-down-s-line',
+  menu: 'ri-menu-line',
+  subgroup: 'ri-arrow-down-s-fill',
+  dropdown: 'ri-arrow-down-s-line',
+  radioOn: 'custom-radio-checked',
+  radioOff: 'custom-radio-unchecked',
+  edit: 'ri-pencil-line',
+  ratingEmpty: 'custom-star-empty',
+  ratingFull: 'custom-star-fill',
+  ratingHalf: 'custom-star-half',
+  loading: 'ri-refresh-line',
+  first: 'ri-skip-left-line',
+  last: 'ri-skip-right-line',
+  unfold: 'ri-split-cells-vertical',
+  file: 'ri-attachment-2',
+  plus: 'ri-add-line',
+  minus: 'ri-subtract-line',
+  sortAsc: 'ri-arrow-up-line',
+  sortDesc: 'ri-arrow-down-line',
 }
-
-const modifiedAliases = Object.assign(aliases, alertTypeIcon)
 
 export const iconify = {
   component: (props: any) => h(
@@ -25,7 +56,7 @@ export const iconify = {
 
 export const icons = {
   defaultSet: 'iconify',
-  mergedAliases: modifiedAliases,
+  aliases,
   sets: {
     iconify,
   },

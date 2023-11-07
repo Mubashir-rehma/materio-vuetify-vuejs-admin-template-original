340dd4f86cf9e5008e405a95312761b7d5f772ee
Deep Kumbhare <kumbharedeep85@gmail.com>
2023-11-07 18:16:19 +0530

    fix(button): fixed button issue

--- typescript-version/full-version/src/views/demos/components/button/DemoButtonGroup.vue
+++ typescript-version/full-version/src/views/demos/components/button/DemoButtonGroup.vue
@@ -9,9 +9,21 @@ const toggleExclusive = ref(1)
     color="primary"
     variant="outlined"
   >
-    <VBtn icon="mdi-format-align-left" />
-    <VBtn icon="mdi-format-align-center" />
-    <VBtn icon="mdi-format-align-right" />
-    <VBtn icon="mdi-format-align-justify" />
+    <VBtn
+      icon="ri-align-left"
+      height="40"
+    />
+    <VBtn
+      icon="ri-align-center"
+      height="40"
+    />
+    <VBtn
+      icon="ri-align-right"
+      height="40"
+    />
+    <VBtn
+      icon="ri-align-justify"
+      height="40"
+    />
   </VBtnToggle>
 </template>

703181999aff572a28b137b7e0ba7fbcbf3fa545
alokVishu <alokpixinvent@gmail.com>
2024-01-01 13:39:32 +0530

    refactor(customizer): update position of app-customizer-toggler

--- typescript-version/full-version/src/@core/components/TheCustomizer.vue
+++ typescript-version/full-version/src/@core/components/TheCustomizer.vue
@@ -555,7 +555,7 @@ const resetCustomizer = async () => {
 
 .app-customizer-toggler {
   position: fixed !important;
-  inset-block-start: 180px;
+  inset-block-start: 20%;
   inset-inline-end: 0;
   transform: translateY(-50%);
 }

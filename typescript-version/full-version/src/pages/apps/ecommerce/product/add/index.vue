47f3e635fdbc2e934fa3bba2df0828f5c41d665a
Deep Kumbhare <kumbharedeep85@gmail.com>
2024-01-05 11:55:46 +0530

    refactor(DropZone): created DropZone component for product add page.

--- typescript-version/full-version/src/pages/apps/ecommerce/product/add/index.vue
+++ typescript-version/full-version/src/pages/apps/ecommerce/product/add/index.vue
@@ -1,48 +1,7 @@
 <script setup lang="ts">
-import { useDropZone, useFileDialog, useObjectUrl } from '@vueuse/core'
 import { ref } from 'vue'
 
 const optionCounter = ref(1)
-
-const dropZoneRef = ref<HTMLDivElement>()
-interface FileData {
-  file: File
-  url: string
-}
-
-const fileData = ref<FileData[]>([])
-const { open, onChange } = useFileDialog({ accept: 'image/*' })
-
-function onDrop(DroppedFiles: File[] | null) {
-  DroppedFiles?.forEach(file => {
-    if (file.type.slice(0, 6) !== 'image/') {
-      // eslint-disable-next-line no-alert
-      alert('Only image files are allowed')
-
-      return
-    }
-
-    fileData.value.push({
-      file,
-      url: useObjectUrl(file).value ?? '',
-    })
-  },
-  )
-}
-
-onChange((selectedFiles: any) => {
-  if (!selectedFiles)
-    return
-
-  for (const file of selectedFiles) {
-    fileData.value.push({
-      file,
-      url: useObjectUrl(file).value ?? '',
-    })
-  }
-})
-
-useDropZone(dropZoneRef, onDrop)
 
 const content = ref(
   `<p>
@@ -154,86 +113,7 @@ const inventoryTabsData = [
           </VCardItem>
 
           <VCardText>
-            <div class="flex">
-              <div class="w-full h-auto relative">
-                <div
-                  ref="dropZoneRef"
-                  class="cursor-pointer"
-                  @click="() => open()"
-                >
-                  <div
-                    v-if="fileData.length === 0"
-                    class="d-flex flex-column justify-center align-center gap-y-3 pa-5 border-dashed drop-zone"
-                  >
-                    <IconBtn
-                      variant="tonal"
-                      class="rounded-sm"
-                    >
-                      <VIcon icon="mdi-upload" />
-                    </IconBtn>
-                    <div class="text-base text-high-emphasis font-weight-medium">
-                      Drag and Drop Your Image Here.
-                    </div>
-                    <span class="text-disabled">or</span>
-
-                    <VBtn variant="outlined">
-                      Browse Images
-                    </VBtn>
-                  </div>
-
-                  <div
-                    v-else
-                    class="d-flex justify-center align-center gap-3 pa-8 border-dashed drop-zone flex-wrap"
-                  >
-                    <VRow class="match-height w-100">
-                      <template
-                        v-for="(item, index) in fileData"
-                        :key="index"
-                      >
-                        <VCol
-                          cols="12"
-                          sm="4"
-                        >
-                          <VCard
-                            :ripple="false"
-                            border
-                          >
-                            <VCardText
-                              class="d-flex flex-column"
-                              @click.stop
-                            >
-                              <VImg
-                                :src="item.url"
-                                width="200px"
-                                height="150px"
-                                class="w-100 mx-auto"
-                              />
-                              <div class="mt-2">
-                                <span class="clamp-text text-wrap">
-                                  {{ item.file.name }}
-                                </span>
-                                <span>
-                                  {{ item.file.size / 1000 }} KB
-                                </span>
-                              </div>
-                            </VCardText>
-                            <VCardActions>
-                              <VBtn
-                                variant="text"
-                                block
-                                @click.stop="fileData.splice(index, 1)"
-                              >
-                                Remove File
-                              </VBtn>
-                            </VCardActions>
-                          </VCard>
-                        </VCol>
-                      </template>
-                    </VRow>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <DropZone />
           </VCardText>
         </VCard>
 

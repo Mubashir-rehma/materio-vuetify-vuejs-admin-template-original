ffde2c375855dca2fa5cd01043db5341127f97c6
Deep Kumbhare <kumbharedeep85@gmail.com>
2024-01-16 11:48:32 +0530

    feat(alert): added a new alert in alert basic demo.

--- typescript-version/full-version/src/views/demos/components/alert/DemoAlertBasic.vue
+++ typescript-version/full-version/src/views/demos/components/alert/DemoAlertBasic.vue
@@ -1,5 +1,13 @@
 <template>
-  <VAlert color="primary">
-    Good Morning! Start your day with some alerts.
-  </VAlert>
+  <div class="demo-space-y">
+    <VAlert color="primary">
+      Good Morning! Start your day with some alerts.
+    </VAlert>
+    <VAlert
+        title="Alert Title"
+        type="error"
+        closable
+        text="Alert Text"
+    />
+  </div>
 </template>

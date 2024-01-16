b4ebbca7e09b8769b1db17c2c701b2638fb20a55
Deep Kumbhare <kumbharedeep85@gmail.com>
2024-01-16 13:08:44 +0530

    refactor(footer): refactored spaces in footer.

--- typescript-version/full-version/src/layouts/components/Footer.vue
+++ typescript-version/full-version/src/layouts/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-100 d-flex align-center justify-space-between">
+  <div class="h-100 d-flex align-center justify-md-space-between justify-center">
     <!-- 👉 Footer: left content -->
     <span class="d-flex align-center">
       &copy;

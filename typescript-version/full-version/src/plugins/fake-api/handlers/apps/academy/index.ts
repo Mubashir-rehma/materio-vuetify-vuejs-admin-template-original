fe843976c0547288eaa3787d11d6efae91bb812b
yagnikvamja <yvpatel121@gmail.com>
2024-01-18 11:31:53 +0530

    refactor(academy): Updated API call for academy app

--- typescript-version/full-version/src/plugins/fake-api/handlers/apps/academy/index.ts
+++ typescript-version/full-version/src/plugins/fake-api/handlers/apps/academy/index.ts
@@ -1,15 +1,13 @@
-import { paginateArray } from '@api-utils/paginateArray'
-import { db } from '@db/apps/academy/db'
 import is from '@sindresorhus/is'
 import destr from 'destr'
-import { http, HttpResponse } from 'msw'
-
+import { HttpResponse, http } from 'msw'
+import { db } from '@db/apps/academy/db'
+import { paginateArray } from '@api-utils/paginateArray'
 
 export const handlerAppsAcademy = [
 
   // 👉 Course
   http.get(('/api/apps/academy/courses'), ({ request }) => {
-
     const url = new URL(request.url)
 
     const q = url.searchParams.get('q')
@@ -68,34 +66,15 @@ export const handlerAppsAcademy = [
       }
     }
 
-    // return res(
-    //   ctx.status(200),
-    //   ctx.json({
-    //     courses: paginateArray(filteredCourses, itemsPerPageLocal, pageLocal),
-    //     total: filteredCourses.length,
-    //   }),
-    // )
-
-    // return HttpResponse.json({
-    //   data: {
-    //     courses: filteredCourses,
-    //     total: filteredCourses.length,
-    //   },
-    // })
-
     return HttpResponse.json({
-        courses: paginateArray(filteredCourses, itemsPerPageLocal, pageLocal),
-        total: filteredCourses.length,
-    })
-    
+      courses: paginateArray(filteredCourses, itemsPerPageLocal, pageLocal),
+      total: filteredCourses.length,
+    },
+    { status: 200 })
   }),
 
   // 👉 Course Details
   http.get(('/api/apps/academy/course-details'), () => {
-    return HttpResponse.json({
-
-        courseDetails: db.courseDetails,
-
-    })
+    return HttpResponse.json(db.courseDetails, { status: 200 })
   }),
 ]

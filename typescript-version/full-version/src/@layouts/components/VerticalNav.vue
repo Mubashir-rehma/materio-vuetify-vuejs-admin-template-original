fa91b85b962eebf0f5a43c06750b6f483807eb99
jd-solanki <jdsolanki0001@gmail.com>
2024-03-15 17:12:19 +0530

    feat(layouts): add after-nav-items slot

--- typescript-version/full-version/src/@layouts/components/VerticalNav.vue
+++ typescript-version/full-version/src/@layouts/components/VerticalNav.vue
@@ -136,6 +136,7 @@ const hideTitleAndIcon = configStore.isVerticalNavMini(isHovered)
         />
       </PerfectScrollbar>
     </slot>
+    <slot name="after-nav-items" />
   </Component>
 </template>
 
@@ -219,7 +220,7 @@ const hideTitleAndIcon = configStore.isVerticalNavMini(isHovered)
 }
 
 // Small screen vertical nav transition
-@media (max-width:1279px) {
+@media (max-width: 1279px) {
   .layout-vertical-nav {
     &:not(.visible) {
       transform: translateX(-#{variables.$layout-vertical-nav-width});

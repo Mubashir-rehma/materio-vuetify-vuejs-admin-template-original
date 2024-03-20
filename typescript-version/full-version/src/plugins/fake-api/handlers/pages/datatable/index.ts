4e7724f561ed423f21fddf9679df86712d6eb772
Deep Kumbhare <kumbharedeep85@gmail.com>
2024-03-20 13:54:09 +0530

    refactor(api): added comments for api handlers.

--- typescript-version/full-version/src/plugins/fake-api/handlers/pages/datatable/index.ts
+++ typescript-version/full-version/src/plugins/fake-api/handlers/pages/datatable/index.ts
@@ -1,6 +1,7 @@
 import { HttpResponse, http } from 'msw'
 import { db } from '@db/pages/datatable/db'
 
+// Handler for pages/datatable
 export const handlerPagesDatatable = [
   http.get(('/api/pages/datatable'), () => {
     return HttpResponse.json(db.salesDetails,

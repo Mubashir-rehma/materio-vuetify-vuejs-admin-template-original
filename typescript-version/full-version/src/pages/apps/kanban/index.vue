fba4da4303a763d7c941e92eb0bce67d10c2db33
alokVishu <alok@themeselection.com>
2024-03-26 17:43:09 +0530

    kanban

---
+++ typescript-version/full-version/src/pages/apps/kanban/index.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { useDragAndDrop } from '@formkit/drag-and-drop/vue'
+import KanbanBoard from './kanban-board.vue'
+
+const todoItems = ['Schedule perm', 'Rewind VHS tapes', 'Make change for the arcade', 'Get disposable camera developed', 'Learn C++', 'Return Nintendo Power Glove']
+const doneItems = ['Pickup new mix-tape from Beth']
+
+const kanban = [
+  {
+    id: 1,
+    name: 'Not Stated',
+    tasks: todoItems,
+  },
+  {
+    id: 2,
+    name: 'In Progress',
+    tasks: doneItems,
+  },
+  {
+    id: 3,
+    name: 'Done',
+    tasks: doneItems,
+  },
+]
+
+const [kanbanWrapper, todo] = useDragAndDrop(kanban)
+</script>
+
+<template>
+  <div
+    ref="kanbanWrapper"
+    class="d-flex ga-4"
+  >
+    <template
+      v-for="kb in todo"
+      :key="kb.id"
+    >
+      <KanbanBoard
+        group-name="kanban"
+        :todo-items="kb.tasks"
+        :board-name="kb.name"
+      />
+    </template>
+  </div>
+</template>
---
+++ typescript-version/full-version/src/pages/apps/kanban/kanban-board.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { useDragAndDrop } from '@formkit/drag-and-drop/vue'
+
+const props = defineProps<{
+  todoItems: string[]
+  groupName: string
+  boardName: string
+}>()
+
+const [todoList, todos] = useDragAndDrop(props.todoItems, { group: props.groupName })
+</script>
+
+<template>
+  <div>
+    <h4 class="text-4 mb-3">
+      {{ boardName }}
+    </h4>
+
+    <div
+      ref="todoList"
+      class="pa-4 bg-surface rounded d-flex flex-column gap-4 elevation-4"
+    >
+      <div
+        v-for="todo in todos"
+        :key="todo"
+        class="pa-2 border rounded"
+      >
+        {{ todo }}
+      </div>
+    </div>
+  </div>
+</template>
